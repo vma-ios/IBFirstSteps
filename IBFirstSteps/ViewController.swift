d001d57ee4f2cde2cbaf64b723c0e000787f246c
lubos.ilcik <lubos.ilcik@touch4it.com>
2018-10-18 15:07:55 +0200

    Initial Commit

---
+++ IBFirstSteps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBFirstSteps
+//
+//  Created by Lubos Ilcik on 18/10/2018.
+//  Copyright © 2018 Touch4It s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
